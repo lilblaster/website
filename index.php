e66b0cad1cd1325ea695e8faecfe388f8eb6065f
onejames <onejames@gmail.com>
2016-09-22 10:09:23 -0400

    added index page

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>My Website</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+<!--   <link rel="stylesheet" href="css/styles.css?v=1.0"> -->
+<!--   <script src="js/scripts.js"></script> -->
+
+</head>
+
+<body>
+  <h2>
+    This is my site!
+  </h2>
+</body>
+</html>
